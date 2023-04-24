4a3f1c883c94b2961365fe8621e1a7d07819fbd3
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-24 16:14:40 +0100

    DONE

---
+++ newprintf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - A function that produces output according to a format.
+ * 
+ *
+ *@format: character string parameter
+ *
+ * Return: The number of characters printed
+ */
+
+
